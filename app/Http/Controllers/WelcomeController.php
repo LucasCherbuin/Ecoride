0a0b53600ce8a056b07a57c53e9c8f8e9e33546c
LucasCherbuin <lucas.cherbuin@gmail.com>
2025-03-11 18:20:16 +0100

    correction page d'accueil

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,9 +10,17 @@ class WelcomeController extends Controller
 {
     public function itineraire(itineraireRequest $request)
     {
+        // Crée un nouveau covoiturage avec les données du formulaire
         covoiturage::create([
             'départ' => $request->depart,
             'arrivé' => $request->arrive,
         ]);
+
+        // Redirige vers la page covoiturage avec les paramètres de recherche
+        return redirect()->route('covoiturage', [
+            'depart' => $request->depart,
+            'arrive' => $request->arrive
+        ]);
     }
 }
+
